d701d0600c6da3252a9189f1219b7ef7a7d311bf
Stewz00 <markus.stitka94@gmail.com>
2022-06-11 07:50:13 +0200

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root-wooow',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'xd its working';
+}
