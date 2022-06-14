99c519ede17b8605768923539282dfba25aa6ce2
Stewz00 <markus.stitka94@gmail.com>
2022-06-14 16:45:55 +0200

    refactored for portfolio design

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DataProtectionComponent } from './pages/data-protection/data-protection/data-protection.component';
 import { ImprintComponent } from './pages/imprint/imprint/imprint.component';
-import { SlideshowComponent } from './shared/slideshow/slideshow.component';
+
 
 const routes: Routes = [
-  { path: '', component: SlideshowComponent },
+// home component:   { path: '', component:  },
   { path: 'imprint', component: ImprintComponent },
   { path: 'data-protection', component: DataProtectionComponent },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SlideshowComponent } from './shared/slideshow/slideshow.component';
 import { NavMenuComponent } from './core/components/nav-menu/nav-menu.component';
 import { ImprintComponent } from './pages/imprint/imprint/imprint.component';
 import { DataProtectionComponent } from './pages/data-protection/data-protection/data-protection.component';
+import { FooterComponent } from './core/components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SlideshowComponent,
     NavMenuComponent,
     ImprintComponent,
-    DataProtectionComponent
+    DataProtectionComponent,
+    FooterComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
